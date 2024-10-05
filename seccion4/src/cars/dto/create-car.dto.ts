d096529012a8eb4bcf7db1e75293041f4cb935ed
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2024-10-05 13:50:59 -0600

    seccion9 deploy

--- seccion4/src/cars/dto/create-car.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class CreateCarDto {
-    @IsString()
-    public readonly brand : string;
-
-    @IsString()
-    public readonly model : string;
-}
