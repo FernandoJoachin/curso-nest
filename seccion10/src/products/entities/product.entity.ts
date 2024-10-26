25a93371062bf67975fd43e66ed96a17e2d2c1c8
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2024-10-26 02:33:26 -0600

    seccion10 completada

--- seccion10/src/products/entities/product.entity.ts
+++ seccion10/src/products/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Product {
@@ -10,7 +10,7 @@ export class Product {
     })
     title : string;
 
-    @Column('numeric',{
+    @Column('float',{
         default: 0
     })
     price : number;
@@ -39,4 +39,27 @@ export class Product {
     @Column('text')
     gender : string;
 
+    @Column('text', {
+        array: true,
+        default: []
+    })
+    tags: string[];
+
+    @BeforeInsert()
+    checkSlugInsert(){
+        if(!this.slug) {
+            this.slug = this.title;
+        }
+
+        this.slug = this.slug.toLowerCase()
+            .replaceAll(' ','_')
+            .replaceAll("'",'')
+    }
+
+    @BeforeUpdate()
+    checkSlugUpdate(){
+        this.slug = this.slug.toLowerCase()
+            .replaceAll(' ','_')
+            .replaceAll("'",'')
+    }
 }
