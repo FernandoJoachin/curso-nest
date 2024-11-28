981e67a16608fa2920e7e58965aa4ec3e7442ed6
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2024-11-28 00:45:53 -0600

    seccion13 avance

--- seccion13/src/auth/auth.controller.ts
+++ seccion13/src/auth/auth.controller.ts
@@ -1,15 +1,55 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, Headers, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service'
-import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto, CreateUserDto } from './dto';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from './entities/user.entity';
+import { RawHeaders, GetUser } from './decorators';
+import { IncomingHttpHeaders } from 'http';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  create(@Body() createUserDto: CreateUserDto) {
+  createUser(@Body() createUserDto: CreateUserDto) {
     return this.authService.create(createUserDto);
   }
 
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto);
+  }
 
+  @Get('private')
+  @UseGuards(AuthGuard())
+  testingPrivateRoute(
+    @Req() request : Express.Request,
+    @GetUser() user : User,
+    @GetUser('email') userEmail : string,
+    
+    @RawHeaders() rawHeaders : string[],
+    @Headers() headers : IncomingHttpHeaders,
+  ){
+    return {
+      ok: true,
+      message: 'Hola Mundo Private',
+      user,
+      userEmail,
+      rawHeaders,
+      headers
+    }
+  }
+
+  @Get('private2')
+  @SetMetadata('roles', ['admin','super-user'])
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  privateRoute2(
+    @GetUser() user : User
+  ){
+    return {
+      ok: true,
+      user
+    }
+  }
 }
