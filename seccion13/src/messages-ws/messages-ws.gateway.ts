988fc57213383462eb69da8fc8b54144ad3bac5b
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2024-12-15 02:31:22 -0600

    seccion15 completada

--- seccion13/src/messages-ws/messages-ws.gateway.ts
+++ seccion13/src/messages-ws/messages-ws.gateway.ts
@@ -1,17 +1,58 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dto/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/auth/interfaces';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+  @WebSocketServer() wss : Server;
+
+  constructor(
+    private readonly messagesWsService : MessagesWsService,
+    private readonly jwtService : JwtService
+  ) {}
   
-  handleConnection(client: Socket) {
+  async handleConnection(client: Socket) {
     //console.log('Cliente conectado:', client.id);
-    this.messagesWsService.registerClient(client);
+    const token = client.handshake.headers.authentication as string;
+    let payload : JwtPayload;
+
+    try {
+      payload = this.jwtService.verify( token );
+      await this.messagesWsService.registerClient(client, payload.id);
+
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
   }
   handleDisconnect(client: Socket) {
     //console.log('Cliente desconectado', client.id);
     this.messagesWsService.removeClient(client.id);
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
+  }
+
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient(client : Socket, payload : NewMessageDto) {
+    //! Emite únicamente al cliente.
+    /* client.emit('message-from-server', {
+      fullName: 'Soy Yo!',
+      message: payload.message || 'no-message!!'
+    }); */
+
+    //! Emitir a todos MENOS, al cliente inicial
+    /* client.broadcast.emit('message-from-server', {
+      fullName: 'Soy Yo!',
+      message: payload.message || 'no-message!!'
+    }); */
+
+    this.wss.emit('message-from-server', {
+      fullName: this.messagesWsService.getUserFullName(client.id),
+      message: payload.message || 'no-message!!'
+    });
   }
 }
--- ws-client/src/socket-client.ts
+++ ws-client/src/socket-client.ts
@@ -1,14 +1,26 @@
 import { Manager, Socket } from "socket.io-client";
 
-export const connectToServer = () => {
-    const manager = new Manager('http://localhost:3000/socket.io/socket.io.js');
+let socket : Socket;
 
-    const socket = manager.socket('/');
-    addListeners(socket);
+export const connectToServer = (token : string) => {
+    const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
+        extraHeaders: {
+            hola: 'mundo',
+            authentication: token
+        }
+    });
+
+    socket?.removeAllListeners();
+    socket = manager.socket('/');
+    addListeners();
 }
 
-const addListeners = (socket : Socket) => {
+const addListeners = () => {
     const serverStatusLabel = document.querySelector('#server-status')!;
+    const clientsUl = document.querySelector('#clients-ul')!;
+    const messageForm = document.querySelector<HTMLFormElement>('#message-form')!;
+    const messageInput = document.querySelector<HTMLInputElement>('#message-input')!;
+    const messagesUl = document.querySelector<HTMLUListElement>('#messages-ul')!;
 
     socket.on('connect', () => {
         serverStatusLabel.innerHTML = 'connected';
@@ -16,5 +28,39 @@ const addListeners = (socket : Socket) => {
 
     socket.on('disconnect', () => {
         serverStatusLabel.innerHTML = 'disconnected';
+    });
+
+    socket.on('clients-updated', (clients : string[]) => {
+        let clientsHtml = '';
+        clients.forEach(clientId => {
+            clientsHtml += `
+                <li>${clientId}</li>
+            `
+        });
+        clientsUl.innerHTML = clientsHtml;
+    });
+
+    messageForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        if(messageInput.value.trim().length <= 0) return;
+
+        socket.emit('message-from-client', { 
+            id: 'YO!!', 
+            message: messageInput.value 
+        });
+
+        messageInput.value = '';
+    });
+
+    socket.on('message-from-server', (payload: { fullName: string, message: string }) => {
+        const newMessage = `
+            <li>
+                <strong>${payload.fullName}</strong>
+                <span>${payload.message}</span>
+            </li>
+        `;
+        const li = document.createElement('li');
+        li.innerHTML = newMessage;
+        messagesUl.append(li);
     });
 }
